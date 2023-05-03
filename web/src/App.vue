e595209859d3552417482e3531d7f993e18675b5
koni <test@q.com>
2023-05-03 22:37:41 +0800

    init

---
+++ web/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+
+<script setup lang='ts'>
+
+</script>
+
+<style lang="less">
+html, body, #app {
+  height: 100%;
+  width: 100%;
+}
+</style>
---
+++ web/src/main.ts
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import './assets/css/reset.less'
+import App from './App.vue'
+
+const app = createApp(App)
+app.mount('#app')
