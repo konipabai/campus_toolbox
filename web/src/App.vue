5c9ace768e6357bf86b903bb3611ff47d0e6ab6d
koni <test@q.com>
2023-05-04 22:59:14 +0800

    总体布局

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,6 +1,12 @@
 <template>
-  <div>
-    
+  <div class="box">
+    <el-container class="box-main">
+      <el-aside width="230px">Aside</el-aside>
+      <el-container>
+        <el-header height="50px">Header</el-header>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
@@ -12,5 +18,11 @@
 html, body, #app {
   height: 100%;
   width: 100%;
+}
+.box {
+  height: 100%;
+  &-main {
+    height:100%
+  }
 }
 </style>
--- web/src/main.ts
+++ web/src/main.ts
@@ -1,6 +1,14 @@
 import { createApp } from 'vue'
 import './assets/css/reset.less'
 import App from './App.vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import { createPinia } from 'pinia'
 
+const pinia = createPinia()
 const app = createApp(App)
+
+app.use(ElementPlus)
+app.use(pinia)
+
 app.mount('#app')
