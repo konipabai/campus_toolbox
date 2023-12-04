da2e3ca5441503b0947ed9cb35aa365f7656a1a4
koni <2413621979@qq.com>
2023-12-04 19:59:36 +0800

    增加用户信息数据库

---
+++ node/src/user/dto/user.dto.ts
@@ -0,0 +1,11 @@
+// 数据提供者
+// 数据库: DB = database
+// 前端:   FE = frontend
+// 后端:   BE = backend
+
+// 数据库查询出来的用户数据
+export interface DB_resultUserDto {
+    account: string;
+    name: string;
+    class: string;
+}
--- node/src/user/entities/user.entity.ts
+++ node/src/user/entities/user.entity.ts
@@ -1,1 +1,16 @@
-export class User {}
+import { Entity, Column, PrimaryGeneratedColumn } from "typeorm"
+
+@Entity()
+export class resultUser {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ type: 'varchar', length: 255, nullable: false })
+    account: string;             // 账号
+
+    @Column({ type: 'varchar', length: 255, nullable: false })
+    name: string;                // 用户名
+
+    @Column({ type: 'varchar', length: 255, nullable: false })
+    class: string;                // 班级
+}
