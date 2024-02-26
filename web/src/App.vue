b5218b70f3c5468b01569458bec1b26dbcafe6b5
koni <2413621979@qq.com>
2024-02-26 20:21:16 +0800

    添加登录

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,13 +1,33 @@
 <template>
-  <box></box>
+  <component :is="currentComponent"></component>
 </template>
 
 <script setup lang='ts'>
+import { ref, watch, markRaw } from "vue";
+import { useRoute } from "vue-router";
 import box from "./layout/box.vue";
+import login from "./components/contents/login/index.vue";
+
+const currentComponent: any = ref(null);
+const route = useRoute();
+
+const updateCurrentComponent = () => {
+  if (route.path === "/") {
+    currentComponent.value = markRaw(login);
+  } else {
+    currentComponent.value = markRaw(box);
+  }
+}
+
+updateCurrentComponent;
+
+watch(route, updateCurrentComponent);
 </script>
 
 <style lang="less">
-html, body, #app {
+html,
+body,
+#app {
   height: var(--element-height-full);
   width: var(--element-width-full);
 }
